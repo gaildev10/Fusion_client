5cccdd61c5392023e2c843f8b7810bb6305e8e89
GailDev10 <GailDev10@gmail.com>
2022-06-07 08:23:14 -0400

    added info to try-catch

--- artist.js
+++ artist.js
@@ -2,15 +2,20 @@ document.querySelector('button').addEventListener('click', apiRequest )
 
 async function apiRequest(){
     const groupName = document.querySelector('input').value
+    
     try{
         const response = await fetch(`https://simple-fusion-api.herokuapp.com/api/${groupName}`)
         const data = await response.json()
 
         document.querySelector('.groupName').innerText=data.groupName
         document.querySelector('.yearsActive').innerText=data.yearsActive
-        document.querySelector('.musicians').innerText=data.musicians
+        document.querySelector('.recordLabel').innerText=data.recordLabel
+        document.querySelector('.musicians').innerText=data.musicians['musicianName']
         document.querySelector('.releases').innerText=data.releases
-        document.querySelector('.groupCaption').src=data.groupCaption
+        document.querySelector('#groupImage').src=data.groupImage
+        document.querySelector('#groupCaption').innerText=data.groupCaption
+       
+
 
     } catch(error) {
         console.log(error)
